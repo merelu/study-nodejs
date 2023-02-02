83e78f61d928d26c45af722bedb9058d0fb7cb63
gyuha <gyuha0310@gmail.com>
2023-02-02 15:17:25 +0900

    stream basic

---
+++ stream/gzip-send.js
@@ -0,0 +1,35 @@
+import { createCipheriv, randomBytes } from "crypto";
+import { createReadStream } from "fs";
+import { request } from "http";
+import { basename } from "path";
+import { createGzip } from "zlib";
+
+const filename = process.argv[2];
+const serverHost = process.argv[3];
+const secret = Buffer.from(process.argv[4], "hex");
+const iv = randomBytes(16);
+
+const httpRequestOptions = {
+  hostname: serverHost,
+  port: 3000,
+  path: "/",
+  method: "PUT",
+  headers: {
+    "Content-Type": "application/octec-stream",
+    "Content-Encoding": "gzip",
+    "X-Filename": basename(filename),
+    "X-Initialization-Vector": iv.toString("hex"),
+  },
+};
+
+const req = request(httpRequestOptions, (res) => {
+  console.log(`Server response: ${res.statusCode}`);
+});
+
+createReadStream(filename)
+  .pipe(createGzip())
+  .pipe(createCipheriv("aes192", secret, iv))
+  .pipe(req)
+  .on("finish", () => {
+    console.log("File Successfully sent");
+  });
